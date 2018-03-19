ec57fe5e86863c3e2ec075bfe6b53e17efbfd25e
Mateusz Koszutowski <mkoszutowski@divante.pl>
2018-03-19 13:55:50 +0100

    WIP

--- src/Bliskapaczka/ApiClient/Validator/Order/Advice/Sender.php
+++ src/Bliskapaczka/ApiClient/Validator/Order/Advice/Sender.php
@@ -15,6 +15,18 @@ use Bliskapaczka\ApiClient\Exception;
  */
 class Sender extends Advice implements ValidatorInterface
 {
+    protected $properties = [
+        'senderEmail' => ['maxlength' => 60, 'notblank' => true],
+        'senderPhoneNumber' => ['notblank' => true],
+        'senderPostCode' => ['notblank' => true],
+        'senderFirstName' => ['maxlength' => 30, 'notblank' => true],
+        'senderLastName' => ['maxlength' => 30, 'notblank' => true],
+        'senderStreet' => ['maxlength' => 30, 'notblank' => true],
+        'senderBuildingNumber' => ['maxlength' => 10, 'notblank' => true],
+        'senderFlatNumber' => ['maxlength' => 10],
+        'senderCity' => ['maxlength' => 30, 'notblank' => true],
+    ];
+
     /**
      * Basic validation for data
      */
--- tests/unit/Bliskapaczka/ApiClient/Validator/Order/Advice/SenderTest.php
+++ tests/unit/Bliskapaczka/ApiClient/Validator/Order/Advice/SenderTest.php
@@ -19,25 +19,7 @@ class SenderTest extends TestCase
             "senderBuildingNumber" => "string",
             "senderFlatNumber" => "string",
             "senderPostCode" => "54-130",
-            "senderCity" => "string",
-            "receiverFirstName" => "string",
-            "receiverLastName" => "string",
-            "receiverPhoneNumber" => "600555432",
-            "receiverEmail" => "eva@example.com",
-            "operatorName" => "INPOST",
-            "destinationCode" => "KRA010",
-            "postingCode" => "KRA011",
-            "codValue" => 0,
-            "insuranceValue" => 0,
-            "additionalInformation" => "string",
-            "parcel" => [
-                "dimensions" => [
-                    "height" => 20,
-                    "length" => 20,
-                    "width" => 20,
-                    "weight" => 2
-                ]
-            ]
+            "senderCity" => "string"
         ];
     }
 
